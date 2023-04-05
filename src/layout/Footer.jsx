35cd5b00ce3924604480cd0fab0a8d99a913ca03
Andrew Kalinichenko <nzeo2008@gmail.com>
2023-04-05 13:04:34 +0700

    Added SortingMenu component

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,35 +1,13 @@
+import SortingMenu from './SortingMenu';
 import { useContext } from 'react';
 import { DataContext } from '../App';
-import Button from '../components/Button/Button';
-import { moveToUp } from '../services/common.service';
 import './footer.css';
 
 function Footer() {
-	const { prevPage, nextPage, data, index, changeToTreeView } =
-		useContext(DataContext);
+	const { changeToTreeView } = useContext(DataContext);
 	return (
 		<footer className="footer-wrapper">
-			<Button
-				className="footer-wrapper-button"
-				disabled={changeToTreeView || index.start === 0}
-				onClick={() => {
-					prevPage();
-					moveToUp();
-				}}
-			>
-				Предыдущая страница
-			</Button>
-
-			<Button
-				className="footer-wrapper-button"
-				disabled={changeToTreeView || index.end >= data.length - 1}
-				onClick={() => {
-					nextPage();
-					moveToUp();
-				}}
-			>
-				Следующая страница
-			</Button>
+			{!changeToTreeView && <SortingMenu />}
 		</footer>
 	);
 }
